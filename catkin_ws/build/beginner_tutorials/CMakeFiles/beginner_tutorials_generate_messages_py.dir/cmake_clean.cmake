fbe041c127d53f9b34e2aa4f0c6b8252a312634c
Brian Wilcox <wilcox.b@husky.neu.edu>
2017-01-12 16:29:37 -0600

    catkin_ws contains beginner_tutorials for Homework Assignment Number 2

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/beginner_tutorials_generate_messages_py"
+  "/home/viki/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/_AddTwoInts.py"
+  "/home/viki/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
