302699998abfd4c866af74347acdc3f89bb6618f
Brian Wilcox <wilcox.b@husky.neu.edu>
2017-01-13 19:13:37 -0600

    Made some modifications to the code (for humor)

--- catkin_ws/src/beginner_tutorials/scripts/add_two_ints_server.py
+++ catkin_ws/src/beginner_tutorials/scripts/add_two_ints_server.py
@@ -1,17 +1,16 @@
 #! /usr/bin/env python
-#Source Code from: http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28python%29
 import roslib; roslib.load_manifest('beginner_tutorials')
 from beginner_tutorials.srv import *
 import rospy
 
 def handle_add_two_ints(req):
-    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    print "Returning [%s + %s = %s] (Next Time just use a Calculator)"%(req.a, req.b, (req.a + req.b))
     return AddTwoIntsResponse(req.a + req.b)
 
 def add_two_ints_server():
     rospy.init_node('add_two_ints_server')
     s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
-    print "Ready to add two ints."
+    print "Ready to add two ints. Is this really a good use of ROS?"
     rospy.spin()
 
 if __name__ == "__main__":
