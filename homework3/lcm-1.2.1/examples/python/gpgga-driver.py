1ddbb5033d39c4493ff7181dfc25c5386e63f7d8
Brian Wilcox <wilcox.b@husky.neu.edu>
2017-01-25 20:38:50 -0600

    For some reason, my gpgga-driver.py code was not under version

--- homework3/lcm-1.2.1/examples/python/gpgga-driver.py
+++ homework3/lcm-1.2.1/examples/python/gpgga-driver.py
@@ -11,18 +11,25 @@ ser = serial.Serial('/dev/ttyUSB0', 4800, timeout=1)
 
 initial = time.time()
 elapsed = 0
-while elapsed < 600:
-	line = ser.readline()
-	line=line.split(',')
+while elapsed < 75:
+	raw = ser.readline()
+	line=raw.split(',')
 	if line[0] == "$GPGGA":
+		print(raw)
 		msg = gpgga_data()
         	msg.timestamp = float(line[1])
         	msg.latitude = float(line[2])
+		utm_lat = float(int(msg.latitude/100)+ (msg.latitude % 100)/60)
         	msg.direction1 = line[3]
+		if line[3] == "S":
+			utm_lat = utm_lat * -1
         	msg.longitude = float(line[4])
-        	msg.direction2 = line[5]
+		utm_long = float(int(msg.longitude/100)+ (msg.longitude % 100)/60)
+		msg.direction2 = line[5]
+		if line[5] == "W":
+			utm_long = utm_long * -1
 		msg.altitude = float(line[9])
-		u = utm.from_latlon(msg.latitude, msg.longitude)
+		u = utm.from_latlon(utm_lat, utm_long)
                 msg.utm_x = long(u[0])
 		msg.utm_y = long(u[1])
 		lc.publish("GPGGA", msg.encode())
